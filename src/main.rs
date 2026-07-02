91bba87c38bf5bc3f68d8654aa2e8d082c02cf4e
Nevy11 <smongare2004@gmail.com>
2026-07-02 16:34:24 +0300

    update a test commitee file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    routing::get,
+    Router,
+};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    // build our application with a single route
+    let app = Router::new().route("/ping", get(|| async { "pong" }));
+
+    // run it with hyper on localhost:3000
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("Listening on http://{}", addr);
+    let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
