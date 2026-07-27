bd87a9f4a5f4a6449fca0bda9d12d90251e9a2bd
nevy11 <smongare2004@gmail.com>
2026-07-27 13:39:22 +0300

    add tower-governer crate to implement rate limiting preventing brute force attacks

--- src/main.rs
+++ src/main.rs
@@ -101,7 +101,7 @@ async fn main() {
     println!("Listening on http://{}", addr);
 
     let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+    axum::serve(listener, app.into_make_service_with_connect_info::<SocketAddr>()).await.unwrap();
 }
 
 #[derive(serde::Serialize)]
